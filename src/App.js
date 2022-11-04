f20f9bc7bb2793749d4138825a36537b2f4bc28f
GH94GitHub <46788915+GH94GitHub@users.noreply.github.com>
2022-11-04 16:52:18 -0500

    Added Bootstrap and Font Awesome

--- src/App.js
+++ src/App.js
@@ -1,28 +1,21 @@
-import logo from "./logo.svg";
 import "./App.css";
-import axios from "axios";
-import { useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import BaseLayout from "./shared/layouts/BaseLayout/BaseLayout";
 import Login from "./pages/Login/Login";
 import AuthLayout from "./shared/layouts/AuthLayout/AuthLayout";
+import LandingPage from "./pages/LandingPage/LandingPage";
 
 function App() {
-  // var products;
-  // useEffect(() => {
-  //   axios.get("/api/products").then((res) => console.log(res.data));
-  // }, []);
-
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<BaseLayout />}>
-          {/* <Route path="/" element={<LandingPage />} /> */}
+        <Route path="" element={<BaseLayout />}>
+          <Route path="/" element={<LandingPage />} />
         </Route>
 
-        <Route path="/auth" element={<AuthLayout />}>
-          <Route index path="/login" element={<Login />} />
+        <Route path="auth" element={<AuthLayout />}>
+          <Route path="login" element={<Login />} />
         </Route>
       </Routes>
     </BrowserRouter>
