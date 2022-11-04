c79266d52a522f1a72267d3bf07daab9a0cbe87d
GH94GitHub <46788915+GH94GitHub@users.noreply.github.com>
2022-11-04 16:51:25 -0500

    Implemented Skeleton

--- src/shared/Navbar/Navbar.js
+++ src/shared/Navbar/Navbar.js
@@ -1,8 +1,97 @@
+import "./Navbar.css";
+
+import Logo from "../../images/logo.svg";
+import ProfileIcon from "../../images/profile.svg";
+
 function NavBar() {
   return (
-    <>
-      <h1>NavBar</h1>
-    </>
+    <nav className="navbar navbar-light bg-light navbar-expand-lg justify-content-between px-2">
+      {/* Logo */}
+      <img src={Logo} alt="placeholder" width="50px" height="50px" />
+
+      {/* Form */}
+      <form className="form-inline">
+        <div className="input-group my-2 my-sm-0">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Search"
+            aria-label="Search"
+          />
+          <button className="btn btn-outline-primary" type="submit">
+            Search
+          </button>
+        </div>
+      </form>
+
+      {/* NavBar Toggler */}
+      <span>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav m-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Link
+              </a>
+            </li>
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="#"
+                id="navbarDropdown"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                <img
+                  src={ProfileIcon}
+                  alt="Profile"
+                  aria-label="Profile"
+                  height="35px"
+                  width="auto"
+                />
+              </a>
+              <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Action
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Another action
+                  </a>
+                </li>
+                <li>
+                  <hr className="dropdown-divider" />
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Something else here
+                  </a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </span>
+    </nav>
   );
 }
 
